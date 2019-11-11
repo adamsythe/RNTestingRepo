145092f05e2aed6f59f791de285ffb34a932c0b6
adamkatz <adam.katz@proversity.org>
2019-11-11 17:09:16 +0200

    added testingLinearGradient

---
+++ TestingExpoLinearGradient/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
